b85ee06a9093efe4141163e31442099ad346b55b
Eleveny Chen <97559209+ElevenyChen@users.noreply.github.com>
2024-04-26 01:46:47 -0700

    Add files via upload

---
+++ trpg_helper.py
@@ -0,0 +1,31 @@
+import character as oc
+import dice_rolling as dice
+
+def main_menu():
+    """
+    Display the main menu and handle user input.
+    """
+    print("Welcome to the TRPG Helper!\n")
+    oc.Character = oc.initialize_character()
+    while True:
+        print("1. My Character")
+        print("2. Check!")
+        print("3. Quick Dice")
+        print("4. Manage Characters")
+        choice = input("Choose an option: ")
+        if choice == "1":
+            oc.print_character(oc.Character)
+        if choice == "2":
+            dice.check_result(oc.Character)
+        elif choice == "3":
+            dice.roll_dice()
+        elif choice == "4":
+            oc.modify_character(oc.Character)
+        elif choice == "24":
+            print("Battle tool not implemented yet, sorry.\n")
+
+        oc.end_card_check(oc.Character)
+            
+
+if __name__ == "__main__":
+    main_menu()
